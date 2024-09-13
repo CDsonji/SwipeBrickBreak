84ba608a08d1bc1da4adec9683423e8c532deb66
Your Name <you@example.com>
2024-09-13 13:57:58 +0330

    set the dimensions of menu pane

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.swipebrickbreakefficiented {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.swipebrickbreakefficiented to javafx.fxml;
+    exports com.example.swipebrickbreakefficiented;
+}
